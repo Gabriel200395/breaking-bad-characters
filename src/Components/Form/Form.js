67e8916d245e38e31e33698177659a2953802295
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-11-20 16:58:42 -0300

    Pagina Edit Criada

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,17 @@
+import "../../Styles/toEdit.css";
+export default function () {
+  return (
+    <section className="to-edit">
+      <h1>To Edit Carachater</h1>
+      <div className="to-edit-container">
+        <form>
+          <input />
+          <input />
+          <input />
+          <input />
+          <button>Enviar</button>
+        </form>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Editar.js
+++ src/Pages/Editar.js
@@ -1,3 +1,9 @@
+import Form from "../Components/Form/Form";
+
 export default function Editar() {
-  return <h1>Seja bem vindo a nossa pagina de editar</h1>;
+  return (
+    <section>
+      <Form />
+    </section>
+  );
 }
