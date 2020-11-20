da24bca58487a0063818a2207fd9c3a0041d3fad
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-11-20 17:28:52 -0300

    Navbar Atualizada na Pagina toEdit

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,16 +1,20 @@
 import "../../Styles/toEdit.css";
+import Navbar from "../Navbar/Navbar";
 export default function () {
   return (
-    <section className="to-edit">
-      <h1>To Edit Carachater</h1>
-      <div className="to-edit-container">
-        <form>
-          <input />
-          <input />
-          <input />
-          <input />
-          <button>Enviar</button>
-        </form>
+    <section>
+      <Navbar />
+      <div className="to-edit">
+        <h1>To Edit Carachater</h1>
+        <div className="to-edit-container">
+          <form>
+            <input />
+            <input />
+            <input />
+            <input />
+            <button>Enviar</button>
+          </form>
+        </div>
       </div>
     </section>
   );
